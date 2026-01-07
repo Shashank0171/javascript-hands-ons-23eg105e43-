48ef718db2f6791c2ad825116dd4b0c4b0beba78
Shashank0171 <23eg105e43@anurag.edu.in>
2026-01-07 18:44:55 +0530

    Add files via upload

---
+++ Object Operation1.js
@@ -0,0 +1,20 @@
+const user = {
+    id: 101,
+    name: "Ravi",
+    email: "ravi@gmail.com",
+    role: "student",
+    isActive: true
+  }
+  //read and print the user’s name and email
+console.log("name:",user.name)
+console.log('email:',user.email)  
+//Add a new property lastLogin: "2026-01-01"
+user.lastLogin="2026-01-01"
+//Update role from "student" to "admin"
+user.role="admin"
+//Delete the isActive property
+delete user.isActive
+console.log(user)
+//Use Object.keys() to list all remaining fields
+console.log(Object.keys(user))
+  
